fcb4fb58989f9522c20faaa04bacf87e09817648
Hannah Provenza <hprovenza@gmail.com>
2016-04-26 18:39:36 -0400

    Added views and HTML

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+__author__ = 'hannahprovenza'
+
+from django.conf.urls import url
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.post_list, name='post_list'),
+]
+
